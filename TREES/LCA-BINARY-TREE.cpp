692e84f1fc237e5b7cca243382e70fe0d2f15645
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-09-21 14:31:57 +0530

    Implemented LCA

--- TREES/LCA-BINARY-TREE.cpp
+++ TREES/LCA-BINARY-TREE.cpp
@@ -3,54 +3,52 @@
 
 using namespace std;
 
+
   struct TreeNode {
       int val;
       TreeNode *left;
       TreeNode *right;
       TreeNode(int x) : val(x), left(NULL), right(NULL) {}
   };
- 
+
+
 class Solution {
 public:
-    int count = 0;
-    void PerformBfs(TreeNode* root, vector<TreeNode *>& parent) {
-
-        if (root == NULL)
-            return;
-        if (count == 0) {
-            parent[root->val] = NULL;
-            count++;
+    // This function will populate the parent map
+    void findParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent) {
+        if (!root) return;
+        
+        if (root->left) {
+            parent[root->left] = root;
+            findParents(root->left, parent);
         }
-    
-        PerformBfs(root->left, parent);
-        PerformBfs(root->right, parent);
-
-        if (root->left != NULL) {
-            parent[root->left->val] = root;
+        if (root->right) {
+            parent[root->right] = root;
+            findParents(root->right, parent);
         }
-        if (root->right != NULL)
-            parent[root->right->val] = root;
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        unordered_map<TreeNode*, TreeNode*> parent; // Map to store parent nodes
+        //Child to parent mapping is used here
+        findParents(root, parent); // Populate the parent map
 
-        vector<TreeNode *> parent(pow(10, 6));
-        PerformBfs(root, parent);
+        unordered_set<TreeNode*> ancestors; // Set to track ancestors of p
 
-        TreeNode* pvalue = p;
-        TreeNode* qvalue = q;
-
-        while (pvalue != NULL) {
-            while (qvalue!= NULL) {
-                if(pvalue==qvalue){
-                    return pvalue;
-                }
-                qvalue=parent[qvalue->val];
-            }
-
-            pvalue=parent[pvalue->val];
+        // Trace back from p to the root and store all ancestors
+        while (p) {
+            ancestors.insert(p);
+            p = parent[p];
         }
 
-        return root;
+        // Now, trace back from q and the first ancestor found in the set is the LCA
+        while (q) {
+            if (ancestors.count(q)) {
+                return q;
+            }
+            q = parent[q];
+        }
+
+        return nullptr; // Should not reach here if p and q are guaranteed to be in the tree
     }
 };
