0e5be37472bc3decd9187b60f935e833a4533500
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-09-27 21:58:12 +0530

    Definition of diameter in a binary tree

--- TREES/diameter-binary-tree.cpp
+++ TREES/diameter-binary-tree.cpp
@@ -3,18 +3,20 @@
 
 using namespace std;
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-  };
-
-class Solution {
+class Solution
+{
 public:
     int LongestPathLength;
-    int findMaxHeight(TreeNode* root) {
+    int findMaxHeight(TreeNode *root)
+    {
         if (root == NULL)
             return 0;
 
@@ -25,9 +27,18 @@ public:
         return 1 + max(leftHeight, rightHeight);
     }
 
-    int diameterOfBinaryTree(TreeNode* root) {
-            LongestPathLength=0;
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        LongestPathLength = 0;
         findMaxHeight(root);
         return LongestPathLength;
     }
 };
+
+// Diameter of a Binary Tree--> Maximum number of nodes between two nodes A and B
+
+/*
+1.Select two nodes such that the total number of nodes between them is maximized
+2.The total number of nodes between two nodes(including the nodes themselves) will
+give us the actual diameter of the binary tree.
+ */
