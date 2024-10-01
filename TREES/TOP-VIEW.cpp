24b4f7729648c57f7a97f791d69952f24943595a
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-10-01 09:56:22 +0530

    Top view of a binary tree using bfs traversal

---
+++ TREES/TOP-VIEW.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    class Node
+    {
+    public:
+        int data;                               // data field
+        Node *left = nullptr, *right = nullptr; // pointer to the left and right child
+
+        Node() {}
+        Node(int data) : data(data) {}
+        Node(int data, Node *left, Node *right) : data(data), left(left), right(right) {}
+    };
+
+    vector<int> findTopView(Node *root)
+    {
+        // Write your code here...
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        map<int, int> mp; // level position, node val
+
+        queue<pair<Node *, int>> Q; // Node , Level
+        Q.push({root, 0});
+
+        while (!Q.empty())
+        {
+            auto it = Q.front();
+            Q.pop();
+            Node *node = it.first;
+            int level = it.second;
+
+            if (mp.find(level) == mp.end())
+                mp[level] = node->data;
+            if (node->left != NULL)
+                Q.push({node->left, level - 1});
+            if (node->right != NULL)
+                Q.push({node->right, level + 1});
+        }
+
+        for (auto x : mp)
+        {
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
