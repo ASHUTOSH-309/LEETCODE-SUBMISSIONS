7825efbdfd4c2a99bd72fa6425b5ae05bd5d0ecc
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-11-01 07:51:40 +0530

    Using stack the same but SC--->O(3n)

--- STRINGS/fancy-string.cpp
+++ STRINGS/fancy-string.cpp
@@ -1,25 +1,38 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
 class Solution {
 public:
-    string makeFancyString(string s) {
-        string ans = "";
-        ans.push_back(s[0]);
+      string makeFancyString(string s) {
+        stack<char> st;
+        st.push(s[0]);
+
         int n = s.size(),cnt = 1;
         for(int i=1;i<n;i++){
-            if(s[i] == ans.back()){
+            if(s[i] == st.top()){
                 cnt++;
-                // ignore all continuous occurances of an element greater than or equal to 3, by the help of counter 
-                // variable which will state the count of element residing at array back;
-                // even a stack can also be used to perform the same
-                if(cnt < 3)ans.push_back(s[i]);
+                if(cnt < 3) st.push(s[i]);
             }
             else{
                 cnt = 1;
-                ans.push_back(s[i]);
+                st.push(s[i]);
             }
         }
-        return ans;
+        int size=st.size();
+        vector<char> ans(size);
+        long long backIterator=st.size()-1;
+        while(!st.empty()){
+            ans[backIterator]=st.top();
+            st.pop();
+            backIterator--;
+        }
+
+        string myString="";
+        for(char ch: ans){
+            myString+=ch;
+        }
+
+        return myString;
     }
 };
