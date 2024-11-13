69e943d4ef253eb540262420f0ffbd783467eb1a
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-11-13 22:20:12 +0530

    Binary search over the entire array

--- ARRAY/fair-pair.cpp
+++ ARRAY/fair-pair.cpp
@@ -1,24 +1,17 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    long long countFairPairs(vector<int> &nums, int lower, int upper)
-    {
+    long long countFairPairs(vector<int>& v, int lower, int upper) {
         long long ans = 0;
+        sort(v.begin(), v.end());  // Step 1
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                long long sum = nums[i] + nums[j];
-                if (sum >= lower && sum <= upper)
-                    ans++;
-            }
+        for (int i = 0; i < v.size() - 1; i++) {  // Step 2
+            auto up = upper_bound(v.begin() + i + 1, v.end(), upper - v[i]);  // Step 3
+            auto low = lower_bound(v.begin() + i + 1, v.end(), lower - v[i]);  // Step 4
+            ans += (up - low);  // Step 5
         }
-
         return ans;
     }
 };
