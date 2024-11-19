823c4584b1b91c227d5e4a056ebbab84ca1ced2d
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-11-19 09:53:04 +0530

    58 on 93

--- SLIDING-WINDOW/distinct-subarrays-length-k.cpp
+++ SLIDING-WINDOW/distinct-subarrays-length-k.cpp
@@ -2,67 +2,26 @@
 
 using namespace std;
 
-
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
 
-        long long global_max = 0;
-        long long sum = 0;
 
-        map<int, int> CountTracker;
-        int i;
-        bool isFirstSubArrayValid=true;
-        for ( i = 0; i < k; i++) {
-            CountTracker[nums[i]]++;
-            sum += nums[i];
-
-               for(auto it : CountTracker){
-                    if(it.second>1){
-                            isFirstSubArrayValid=false;
-                            break;
+            long long global_max=0;
+            for(int i=0;i<=nums.size()-k;i++){
+                    long long sum=0;
+                    bool AreDistinct=true;
+                    for(int j=i;j<i+k;j++){
+                            sum+=nums[j];
+                            if( (j<i+k-1) && (nums[j]==nums[j+1]) ){
+                                    AreDistinct=false;
+                                    break;
+                            }
                     }
-                }
 
-            if(!isFirstSubArrayValid) break;
-            if(i==k-1) global_max=max(global_max,sum);
-        }
-
-        if(i!=k){
-            CountTracker[nums[i]]++;
-            sum+=nums[i];
-            i++;
-        }
-
-        int j;
-        for ( j = k; j <= nums.size() - 1; j++) {
-
-            sum += nums[j];
-            sum -= nums[j - k];
-            CountTracker[nums[j-k]]--;
-            CountTracker[nums[j]]++;
-
-            // Now sum contains the sum of elements of a k size subarray ending
-            // at i;
-
-// Required to check for all the keys if any of them life having occurence greater than 1
-        
-
-            bool isValidSubarray=true;
-            for(auto it : CountTracker){
-                if(it.second>1){
-                        isValidSubarray=false;
-                        break;
-                }
-
+                    if(AreDistinct) global_max=max(global_max,sum);
             }
 
-            if(!isValidSubarray) continue;
-
-            global_max=max(global_max,sum);
-
-        }
-
-        return global_max;
+            return global_max;
     }
 };
