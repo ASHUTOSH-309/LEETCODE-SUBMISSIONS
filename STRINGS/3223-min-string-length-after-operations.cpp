9c83ca71b3e4b6bac1179b94253f53e616ffea3a
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2025-01-13 16:45:07 +0530

    Odd frequency character will make a final contribution of 1 and even of 2

--- STRINGS/3223-min-string-length-after-operations.cpp
+++ STRINGS/3223-min-string-length-after-operations.cpp
@@ -5,36 +5,17 @@ class Solution {
 public:
     int minimumLength(string s) {
         int n = s.size();
-        int ans = n;
+        int ans = 0;
+        
+        unordered_map<char,long long> mpp;
+        for(int i=0;i<n;i++) mpp[s[i]]++;
 
-        unordered_map<char, set<int>> mppAheadIndex;
-        unordered_map<char, set<int>> mppBeforeIndex;
 
-        for (int i = 0; i < n; i++) {
-            mppAheadIndex[s[i]].insert(i);
-            mppBeforeIndex[s[i]].insert(-i);
-        } 
-
-        for (int i = 1; i < n - 1; i++) {
-            if (s[i] != '*') {
-                auto itAhead = mppAheadIndex[s[i]].upper_bound(i);
-                auto itPrefix = mppBeforeIndex[s[i]].upper_bound(-i);
-
-                int aheadValue = (itAhead != mppAheadIndex[s[i]].end()) ? *itAhead : -1;
-                int prefixValue = (itPrefix != mppBeforeIndex[s[i]].end()) ? -*itPrefix : -1;
-
-                if (aheadValue != -1 && prefixValue != -1) {
-                    s[aheadValue] = '*';
-                    s[prefixValue] = '*';
-                    ans -= 2;
-
-                    // Update the data structures to reflect changes
-                    mppAheadIndex[s[i]].erase(aheadValue);
-                    mppBeforeIndex[s[i]].erase(-prefixValue);
-                }
-            }
+        for(auto it:mpp){
+            if(it.second%2==0) ans+=2;
+            else ans+=1;
         }
-
+        
         return ans;
     }
 };
